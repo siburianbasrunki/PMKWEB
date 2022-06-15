d66692188b5f849ae5cc28e6ec580c0d76cdda90
siburianbasrunki <basrunkisiburian@gmail.com>
2022-06-15 17:12:01 +0700

    nex js bootstrap

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "344eb46fe9b18957"; }
+/******/ 		__webpack_require__.h = function() { return "a42cedf98827e86a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
