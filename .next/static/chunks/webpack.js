0c6c6fb1d59ebd40305429bec1da29ee4d306963
siburianbasrunki <basrunkisiburian@gmail.com>
2022-06-16 11:03:24 +0700

    oke

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a42cedf98827e86a"; }
+/******/ 		__webpack_require__.h = function() { return "cb79390adf577dc8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
